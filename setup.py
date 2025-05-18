9b951272b6a30a8fbd85ac44e580f5776189477c
Rukshan Thantirige <rukshan1015@gmail.com>
2025-05-17 23:30:11 -0500

    removed temp folders/files

--- setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name='myproject',
-    version='0.1',
-    packages=find_packages(),  # auto-discovers any folder with __init__.py
-)
